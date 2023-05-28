0df72db421cd7a29dcdb048874ded4806564fc59
Ali170498 <130515834+Ali170498@users.noreply.github.com>
2023-05-27 23:37:20 -0400

    first commit

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,39 @@
+<x-guest-layout>
+    <x-authentication-card>
+        <x-slot name="logo">
+            <x-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('¿Olvidaste tu contraseña? Ningún problema. Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace de restablecimiento de contraseña que le permitirá elegir una nueva.') }}
+        </div>
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <x-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <div class="block">
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button>
+                    <a class="text-black text-sm dark:text-black hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                        {{ __('Volver') }}
+                    </a>
+                </x-button>
+                <x-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-button>
+            </div>
+        </form>
+    </x-authentication-card>
+</x-guest-layout>
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center">
+    <img src="{{ asset('img/logo.png') }}" alt="Logo de la farmacia" class="h-8 w-auto">
+    <h1 class="text-2xl font-bold ml-2"></h1>
+</div>
